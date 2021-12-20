feb6ff5204e1624388e120d69e76c7d90addd0a1
Tamara Luzija <tamara.luzija@ingemark.com>
2021-12-20 11:50:22 +0100

    Add swagger.json

--- OR-lab3/src/plugins/swagger.ts
+++ OR-lab3/src/plugins/swagger.ts
@@ -16,10 +16,6 @@ export default fp(async (fastify, opts) => {
         description: "Testing the Fastify swagger API",
         version: "0.1.0",
       },
-      externalDocs: {
-        url: "https://swagger.io",
-        description: "Find more info here",
-      },
       host: "localhost:3000",
       schemes: ["http"],
       consumes: ["application/json"],
